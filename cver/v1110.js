3c594bcf647434d9b23c99bc49cf15d881d55da1
bo-blog <info@bo-blog.com>
2016-11-22 00:21:36 +0800

    Update v1110.js

--- cver/v1110.js
+++ cver/v1110.js
@@ -1,3 +1,2 @@
 $("#ins").html("有新版本可用：<b>1.2.1</b><br/>- 分类和自定义模块可以拖动排序了。<br/>- 支持将文件上传到阿里云OSS储存。（特别鸣谢 fanbili 提供测试环境）<br/>- 可以在创建文章时，从中文标题转换出拼音并自动填入文章ID （需支持 mbstring 扩展的服务器）。 <br/>- RESTful API接口，暂支持 articles （日志）的GET接口和 users （作者）的GET接口。<br/>");
-addUpdateBtn ("http://xiaofengtest.qiniudn.com/upkg1110.txt", 'c8472a460ffe55b80b9e0ebee32ecd35');
-
+addUpdateBtn ("http://xiaofengtest.qiniudn.com/upkg1110.txt", 'fc934f7a215f0b95bdb852d3c7f499f9');
