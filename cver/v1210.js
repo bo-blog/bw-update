721518ffe3679eafa3b2282d90ee568066a24800
bo-blog <info@bo-blog.com>
2016-12-18 14:15:10 +0800

    1

--- cver/v1210.js
+++ cver/v1210.js
@@ -1,2 +1,2 @@
 $("#ins").html("有新版本可用：<b>1.3.0</b><br/>- 新增：每分钟自动保存草稿。<br/>- 改进：在输入Tag时，使用中文逗号也能完成添加。<br/>- 改进：在iOS系统下的字体显示问题。<br/>- 新增：可以禁用内部计数功能减少数据库写入操作。<br/>- 微调了默认主题，改进了logo的显示方式和移动版的下拉菜单。<br/>");
-addUpdateBtn ("http://xiaofengtest.qiniudn.com/upkg1210.txt", '0cb5995ee1fa5101f6f14a4b9c74824b');
+addUpdateBtn ("http://xiaofengtest.qiniudn.com/upkg1210.txt", 'b3125b44826ae7369be4bfed4f05b454');
