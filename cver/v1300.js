3bc2a6abc805dc9446287f7b7cc0e8afec7f8d64
bo-blog <info@bo-blog.com>
2017-06-10 13:11:20 +0800

    1.4.0 release

--- cver/v1300.js
+++ cver/v1300.js
@@ -1,1 +1,2 @@
-$("#ins").html("当前版本为最新。<br/>");
+$("#ins").html("有新版本可用：<b>1.4.0</b><br/>- 新增：内置评论回归。取消已停运的多说服务设置。<br/>- 改进：完善了翻页，现在可以快速定位到特定分页。（默认主题）<br/>- 新增：可选择常用搜索引擎作为站内搜索使用。（默认主题）<br/>- 改进：检测到新版时可以将升级包下载下来以便手动更新。<br/><br/><a href='http://xiaofengtest.qiniudn.com/upkg1300.txt' target='_blank'>点这里下载升级包并手动重命名后执行手动更新</a><br/>");
+addUpdateBtn ("http://xiaofengtest.qiniudn.com/upkg1300.txt", '89230739bbf4179ed2f854d0f0c734e0');
