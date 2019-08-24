65f2248e53995dce3187234fa3937e85ad0f5277
bo-blog <info@bo-blog.com>
2019-08-24 23:51:46 +0800

    Add files via upload

--- cver/v1100.js
+++ cver/v1100.js
@@ -1,2 +1,2 @@
 $("#ins").html("有新版本可用：<b>1.1.1</b><br/>- 紧急修复JS错误等。<br/>");
-addUpdateBtn ("http://xiaofengtest.qiniudn.com/upkg1100.txt", '11bf47b9e23f394fd5004e5c8b762eb0');
+addUpdateBtn ("https://bo-blog.github.io/bw-update/package/upkg1100.txt", '11bf47b9e23f394fd5004e5c8b762eb0');
--- cver/v1200.js
+++ cver/v1200.js
@@ -1,2 +1,2 @@
 $("#ins").html("有新版本可用：<b>1.2.1</b><br/>- 分类和自定义模块可以拖动排序了。<br/>- 支持将文件上传到阿里云OSS储存。（特别鸣谢 fanbili 提供测试环境）<br/>- 可以在创建文章时，从中文标题转换出拼音并自动填入文章ID （需支持 mbstring 扩展的服务器）。 <br/>- RESTful API接口，暂支持 articles （日志）的GET接口和 users （作者）的GET接口。<br/>");
-addUpdateBtn ("http://xiaofengtest.qiniudn.com/upkg1200.txt", 'c8472a460ffe55b80b9e0ebee32ecd35');
+addUpdateBtn ("https://bo-blog.github.io/bw-update/package/upkg1200.txt", 'c8472a460ffe55b80b9e0ebee32ecd35');
