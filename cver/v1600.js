7b863c6b59ec34694edcd715eef6bb0765678295
bo-blog <info@bo-blog.com>
2019-08-24 23:41:02 +0800

    Add files via upload

--- cver/v1600.js
+++ cver/v1600.js
@@ -1,1 +1,2 @@
-$("#ins").html("当前版本为最新。<br/>");
+$("#ins").html("有新版本可用：<b>1.7.0</b><br/>- 修复：某些情况下Tag保存和打开异常。<br>- 改进：社交链接增加GitHub。<br>- 新增：第三方评论现在支持GitTalk, Gitment。<br>- 改进：在编辑草稿时，在为草稿选择分类前隐藏发布按钮以避免误解。</br>- 修复：修复了一个只有管理员登陆后才能利用的SQL注入漏洞。<br>- 移除了自带的七牛支持。<br/>");
+addUpdateBtn ("https://github.com/bo-blog/bw-update/raw/gh-pages/package/upkg1600.txt", 'eac8e9df3cc2f8489726f238794ee1bf');
